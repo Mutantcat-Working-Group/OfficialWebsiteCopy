c26d168d8e3413e124870b2a45e6b8c4d212b64f
孙达明 <shun_@outlook.com>
2025-03-11 12:47:08 +0800

    feat:更新网页

---
+++ assets/Show-IpIM-DX2.js
@@ -0,0 +1,70 @@
+import{_ as M}from"./FishMusic-0SevzdNt.js";import{d as A,v as V,f as I,l as N,k as D,o as m,c as L,a as n,b as e,w as t,F as T,r as i,t as f,j as d,i as w,u as y,g as b,m as g,p as G,e as H,_ as U}from"./index-yx7bXgj-.js";import{m as q}from"./marked.esm-afrdLNPY.js";const c=h=>(G("data-v-2937febe"),h=h(),H(),h),P={class:"show"},W=c(()=>n("img",{src:M,style:{width:"300px",height:"300px","border-radius":"30px"}},null,-1)),R={style:{"font-size":"50px","margin-top":"10px"}},X={style:{"font-size":"20px","margin-top":"10px",color:"rgb(129, 129, 129)"}},Z=c(()=>n("p",null,"支持平台：Android",-1)),K={class:"download",id:"download"},Q=c(()=>n("h2",{class:"title"},"产品获取",-1)),Y={class:"update",id:"update"},J=c(()=>n("h2",{class:"title"},"版本日志",-1)),O={style:{height:"50px"}},ee={class:"doc",style:{"margin-bottom":"20px"},id:"doc"},te=c(()=>n("h2",{class:"title"},"产品文档",-1)),oe=c(()=>n("div",{id:"introduce",class:"markdown-body"},null,-1)),ne=A({__name:"Show",setup(h){V.useI18n();const p=I(12),u=I(12);N(()=>{window.addEventListener("resize",function(){var o=window.innerWidth;o<1e3?(p.value=0,u.value=24):(p.value=12,u.value=12)})}),D(()=>{var o=window.innerWidth;o<1e3?(p.value=0,u.value=24):(p.value=12,u.value=12);let a=document.getElementById("introduce");a!=null&&a!=null&&(a.innerHTML=q.parse(`
+<div align="center">
+    <img src="https://s2.loli.net/2025/03/02/IxWZXH5mR6Ed1FY.png" style="width:100px;"/>
+    <h2>鱼乐</h2>
+</div>
+
+### 一、产品概述
+
+- 自挂载的免费无广的网络音乐播放器
+- 支持NAS、云盘(部分)、网络服务器、SSU
+- 支持本地歌单信息导入导出(多端通用)
+- 支持失效资源自动跳过
+
+### 二、安卓版本
+
+1. 用户界面
+   - 播放界面
+   - <img src="https://s2.loli.net/2025/03/11/MdNDnTzSPftL4j5.jpg" alt="img" width="300">
+   - 管理歌单
+   - <img src="https://s2.loli.net/2025/03/11/G5rZF47fuAdUHn8.jpg" alt="img" width="300">
+   - 设置界面
+   - <img src="https://s2.loli.net/2025/03/11/4uUfyASihBlq7zX.jpg" alt="img" width="300">
+2. 播放音乐
+   - 初始界面就是音乐列表界面
+   - 长按底部唱片区域展开播放操作模块
+   - 点击左侧切换列表循环和随机模式
+   - 点击音乐列表左侧的播放键可以直接切换音乐
+3. 导入音乐
+   - 点击右上角的音乐列表按钮跳转至歌单管理界面
+   - 在歌单管理界面上方的下拉框选择歌单
+   - 之后在指定歌单中点击添加音乐进行添加
+   - 填写表格点击添加即可，不可重名，图标可以传入base64或图片链接
+4. 切换歌单
+   - 点击右上角的音乐列表按钮跳转至歌单管理界面
+   - 在歌单管理界面上方的下拉框选择歌单
+   - 点击选用歌单即可切换
+5. 管理歌单
+   - 点击右上角的音乐列表按钮跳转至歌单管理界面
+   - 在歌单管理界面点击右上角加号添加歌单
+6. 设置超时时间
+   - 在主界面点击右上角设置图标
+   - 修改超时时间，点击修改即可
+7. 导入导出
+
+   - 在主界面点击右上角设置图标
+   - 点击导出即可导出本机所有配置和歌单信息
+   - 导出的格式是一个长字符串，可复制保存或分享
+8. 补充说明
+   - 在主界面点击右上角设置图标
+   - 在上方的输入框输入完整正确的数据字符串
+   - 注意！操作前尽量先备份自己的数据，输入数据应确保正确
+   - 导入后将清空本地原有的所有配置和歌单信息，引入新的信息
+
+### 三、免责声明
+
+- 用户本地保存的音频来源与本工具无关
+
+- 用户本地保存的音频来源与本工具无关
+
+- 本应用程序不对第三方内容和应用程序中提供的任何链接或资源负责。
+
+- 数据隐私：明确说明用户数据的处理、存储和保护方式，并保证用户数据不会与第三方共享。
+
+- 责任限制：声明开发者不对因使用该应用程序而产生的任何损害负责。
+
+- 不做保证：明确表示该应用程序不保证所提供服务的准确性、可靠性或可用性。
+
+- 应用更改：告知用户应用程序的功能、界面和政策可能会在不事先通知的情况下进行更改。
+
+        `).toString())});const z=[{version:"1.0.20240311",architecture:"兼容",info:"安装包/APK",md5:"9708d1844c39314b73386520a5854db0",sha1:"161d588d213fb7a16b46e4cf6bf186a414539569",download:{lanzou:"https://shuntaoyuan.lanzout.com/i2G5F2q8v6ti",baidu:"https://pan.baidu.com/s/12NGTD9p8u1w0vQzT_sB8yg?pwd=rwfc"}}];function S(){const o=document.getElementById("download");o&&o.scrollIntoView({behavior:"smooth"})}function $(){const o=document.getElementById("update");o&&o.scrollIntoView({behavior:"smooth"})}function B(){const o=document.getElementById("doc");o&&o.scrollIntoView({behavior:"smooth"})}return(o,a)=>{const _=i("el-col"),s=i("el-button"),v=i("el-row"),r=i("el-table-column"),E=i("el-table"),k=i("el-tab-pane"),x=i("el-tabs"),j=i("el-step"),F=i("el-steps");return m(),L(T,null,[n("div",P,[e(v,{style:{height:"100%"}},{default:t(()=>[e(_,{span:p.value,class:"bg hidden-sm-and-down"},null,8,["span"]),e(_,{span:u.value,style:{display:"flex","flex-direction":"column","align-items":"center","justify-content":"center"}},{default:t(()=>[n("div",null,[e(v,{style:{display:"flex","flex-direction":"column","align-items":"center"}},{default:t(()=>[e(_,{span:24,style:{display:"flex","flex-direction":"column","align-items":"center"}},{default:t(()=>[W,n("h1",R,f(o.$t("apps.FishMusic.FishMusic")),1),n("p",X,f(o.$t("apps.FishMusic.FishMusicInfo")),1)]),_:1}),e(_,{span:24,style:{display:"flex","flex-direction":"row","align-items":"center","justify-content":"center"}},{default:t(()=>[Z]),_:1}),e(_,{span:24,style:{display:"flex","flex-direction":"row","align-items":"center","justify-content":"center"}},{default:t(()=>[e(s,{type:"primary",size:"large",style:{margin:"10px"},onClick:a[0]||(a[0]=l=>S())},{default:t(()=>[d(f(o.$t("apps.acquisition")),1)]),_:1}),e(s,{type:"primary",size:"large",style:{margin:"10px"},onClick:a[1]||(a[1]=l=>$())},{default:t(()=>[d(f(o.$t("apps.versionLog")),1)]),_:1}),e(s,{type:"primary",size:"large",style:{margin:"10px"},onClick:a[2]||(a[2]=l=>B())},{default:t(()=>[d(f(o.$t("apps.document")),1)]),_:1})]),_:1})]),_:1})])]),_:1},8,["span"])]),_:1})]),n("div",K,[Q,e(x,{type:"border-card",class:"downloadbar"},{default:t(()=>[e(k,{label:"Android"},{default:t(()=>[e(v,null,{default:t(()=>[e(E,{data:z,style:{width:"100%"}},{default:t(()=>[e(r,{prop:"version",label:"版本"}),e(r,{prop:"info",label:"形式"}),e(r,{prop:"architecture",label:"架构"}),e(r,{prop:"md5",label:"MD5"}),e(r,{prop:"sha1",label:"SHA1"}),e(r,{label:"下载"},{default:t(l=>[l.row.download.direct!=null?(m(),w(s,{key:0,type:"button.primary",link:"",onClick:C=>y(b)(l.row.download.direct)},{default:t(()=>[d("直接下载")]),_:2},1032,["onClick"])):g("",!0),l.row.download.baidu!=null?(m(),w(s,{key:1,type:"button.primary",link:"",onClick:C=>y(b)(l.row.download.baidu)},{default:t(()=>[d("百度网盘")]),_:2},1032,["onClick"])):g("",!0),l.row.download.lanzou!=null?(m(),w(s,{key:2,type:"button.primary",link:"",onClick:C=>y(b)(l.row.download.lanzou)},{default:t(()=>[d("蓝奏云")]),_:2},1032,["onClick"])):g("",!0),l.row.download.github!=null?(m(),w(s,{key:3,type:"button.primary",link:"",onClick:C=>y(b)(l.row.download.github)},{default:t(()=>[d("Github")]),_:2},1032,["onClick"])):g("",!0)]),_:1})]),_:1})]),_:1})]),_:1})]),_:1})]),n("div",Y,[J,e(x,{type:"border-card",class:"downloadbar"},{default:t(()=>[e(k,{label:"Android"},{default:t(()=>[n("div",O,[e(F,{direction:"vertical",active:2},{default:t(()=>[e(j,{title:"1.0.20240311",description:"初始版本，提供了初始功能"})]),_:1})])]),_:1})]),_:1})]),n("div",ee,[te,e(x,{type:"border-card",class:"downloadbar"},{default:t(()=>[e(k,{label:"功能介绍"},{default:t(()=>[oe]),_:1})]),_:1})])],64)}}}),se=U(ne,[["__scopeId","data-v-2937febe"]]);export{se as default};
