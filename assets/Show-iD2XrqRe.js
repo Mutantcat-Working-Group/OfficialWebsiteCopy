5c78efa4af1202daf2915e3700d880a98e406c69
孙达明 <shun_@outlook.com>
2025-04-01 19:25:39 +0800

    feat:更新

---
+++ assets/Show-iD2XrqRe.js
@@ -0,0 +1,120 @@
+import{_ as A}from"./UCatch-KYQKN88d.js";import{d as B,v as F,f as I,l as Q,k as T,o as m,c as $,a as t,b as e,w as o,F as q,r as d,t as u,j as i,i as w,u as b,n as L,m as v,g as k,p as N,e as X,_ as Z}from"./index-zRfxk9EI.js";import{m as H}from"./marked.esm-afrdLNPY.js";const l=g=>(N("data-v-2172f94c"),g=g(),X(),g),P={class:"show"},W=l(()=>t("img",{src:A,style:{width:"300px",height:"300px","border-radius":"30px"}},null,-1)),G={style:{"font-size":"50px","margin-top":"10px"}},J={style:{"font-size":"20px","margin-top":"10px",color:"rgb(129, 129, 129)"}},R=l(()=>t("p",null,"支持平台：Windows",-1)),O={class:"download",id:"download"},Y=l(()=>t("h2",{class:"title"},"产品获取",-1)),K={class:"buy",id:"buy"},tt=l(()=>t("h2",{class:"title"},"产品定价",-1)),et={class:"sell-card"},ot={class:"card"},lt=l(()=>t("h3",{class:"header",style:{"line-height":"3rem"}},"免费版",-1)),nt=l(()=>t("div",{class:"info"},[t("p",{class:"title"},"包含功能"),t("ul",{class:"list"},[t("li",null,"快捷启动程序"),t("li",null,"管理快捷程序"),t("li",null,"提权操作"),t("li",null,"基础维护服务"),t("li",null,"非强制更新"),t("li",null,"...")])],-1)),st={class:"footer"},at=l(()=>t("p",{class:"tag"},"￥0.00 ",-1)),it={class:"sell-card"},dt={class:"card"},ct=l(()=>t("h3",{class:"header",style:{"line-height":"3rem"}},"激活版",-1)),pt=l(()=>t("div",{class:"info"},[t("p",{class:"title"},"包含功能"),t("ul",{class:"list"},[t("li",null,"免费版所有功能"),t("li",null,"快捷打开文件"),t("li",null,"文件定时备份"),t("li",null,"程序配置备份"),t("li",null,"优先维护服务"),t("li",null,"...")])],-1)),rt={class:"footer"},ut=l(()=>t("p",{class:"tag"},"￥10.10 ",-1)),ht={class:"update",id:"update"},_t=l(()=>t("h2",{class:"title"},"版本日志",-1)),ft={style:{height:"150px"}},mt={class:"doc",style:{"margin-bottom":"20px"},id:"doc"},bt=l(()=>t("h2",{class:"title"},"产品文档",-1)),gt=l(()=>t("div",{id:"introduce",class:"markdown-body"},null,-1)),yt=l(()=>t("div",{class:"user-agreement"},[t("h3",{class:"title"},"软件用户协议"),t("p",null,"欢迎使用本软件！请仔细阅读以下用户协议（以下简称“协议”）。当您使用本软件时，即表示您已同意并接受本协议的所有条款和条件。"),t("p",null,"本软件提供部分功能的免费使用，用户可以通过购买激活码来解锁全部功能。激活码为一次性购买，激活后不可退换。本软件的所有权和知识产权归本公司所有，任何未经授权的复制、修改、分发、转售、反编译、反向工程或其他类似行为均被严格禁止。 "),t("p",null,"用户在使用本软件时，应遵守所有适用的法律法规，并不得利用本软件从事任何违法或不当行为。用户应妥善保管激活码及相关账户信息，如因用户原因导致激活码泄露或丢失，本公司不承担任何责任。"),t("p",null,"我们非常重视用户的隐私和数据安全。用户在使用本软件时，可能需要提供一定的个人信息。本公司将按照隐私政策收集、使用和保护用户的个人信息。具体隐私政策内容请参阅本公司的隐私政策文件。"),t("p",null,"在下列情况下，我们有权在不通知用户的情况下终止本协议并停止用户对软件的使用："),t("li",null,"用户违反本协议的任何条款；"),t("li",null,"用户的行为对本软件或其他用户造成损害；"),t("li",null,"法律要求或政府部门的指示。"),t("p",null,"本软件按“现状”提供，对于软件的适销性、特定用途的适用性或不侵犯知识产权，本公司不作出任何明示或默示的保证。本公司对因使用或无法使用本软件而导致的任何直接、间接、附带、特殊或后果性损害不承担任何责任。 "),t("p",null,"本协议受适用法律管辖。若本协议的任何条款被判定为无效或不可执行，其他条款仍然有效。本公司保留随时修改本协议的权利，修改后的协议将在本公司官方网站上发布，用户有责任定期查看本协议的最新版本。 ")],-1)),wt=l(()=>t("h3",{style:{"margin-bottom":"10px","margin-top":"10px"}},"安全报告 1.0.20250115",-1)),vt=l(()=>t("a",{href:"https://habo.qq.com/file/showdetail?pk=ADEGZ11pB2AIPVs6U2oHZg%3D%3D"},"查看报告",-1)),kt=l(()=>t("br",null,null,-1)),xt=l(()=>t("a",{href:"https://s.threatbook.com/report/file/1595f8a3f1bd8a19379869dd59a089ea75eaf3b28fdfbe4dd1b20d1fb2cecae6"},"查看报告",-1)),Ut=l(()=>t("br",null,null,-1)),Ct=l(()=>t("h3",{style:{"margin-bottom":"10px","margin-top":"10px"}},"安全报告 1.0.20250128 x64",-1)),jt=l(()=>t("a",{href:"https://s.threatbook.com/report/file/8e07329d51984de1dc10d95702ac9592588cfe69fc9b8cf6061192557ebb14a1"},"查看报告",-1)),It=l(()=>t("br",null,null,-1)),Et=l(()=>t("h3",{style:{"margin-bottom":"10px","margin-top":"10px"}},"安全报告 1.0.20250128 x86",-1)),zt=l(()=>t("a",{href:"https://s.threatbook.com/report/file/60dd74a972bba89ba668903d8a62917715b737734c5c3cc2267782f218ad35b1"},"查看报告",-1)),Vt=l(()=>t("br",null,null,-1)),St=B({__name:"Show",setup(g){F.useI18n();const h=I(12),_=I(12);Q(()=>{window.addEventListener("resize",function(){var n=window.innerWidth;n<1e3?(h.value=0,_.value=24):(h.value=12,_.value=12)})}),T(()=>{var n=window.innerWidth;n<1e3?(h.value=0,_.value=24):(h.value=12,_.value=12);let s=document.getElementById("introduce");s!=null&&s!=null&&(s.innerHTML=H.parse(`
+<div align=center>
+<img src="https://s2.loli.net/2025/01/13/tQasPmZpRLgjud4.jpg" style="width:100px;"/>
+<h2>UCatch</h2>
+</div>
+
+### 一、产品概述
+
+- 一款U盘App快捷启动器
+- 将软件安装至U盘（硬盘），从而可以便携软件，便携启动
+- 支持U盘中程序列表快速启动
+- 支持使用中提权
+- 支持自动移除失效程序
+- 支持文件定时备份到U盘
+- 支持程序配置备份与恢复
+- 拖入U盘根目录打开即可使用，无需格盘
+- 单体文件运行，程序文件不超过8M
+
+### 二、界面
+
+1. 软件界面
+   - 快捷程序
+   - ![p1.png](https://s2.loli.net/2025/01/15/RGH7XVejKcQrZvM.jpg)
+   - 快捷打开文件
+   - ![p2.png](https://s2.loli.net/2025/01/15/yZtukadA5mQVfvX.jpg)
+   - 文件定时备份
+   - ![p3.png](https://s2.loli.net/2025/01/15/ptK7f5xq2mRXHPs.jpg)
+   - 程序配置备份
+   - ![p4.png](https://s2.loli.net/2025/01/15/ghq7SBdUoE98p5b.jpg)
+2. 信息界面
+   - 关于界面
+   - ![p5.png](https://s2.loli.net/2025/01/15/ADF45kv9NKQSGcw.jpg)
+
+### 三、功能
+
+1. 快捷程序
+   - 快捷切换
+     - 点击快捷切换时，主页的软件分类框会自动切换为下一个类目
+     - 当切换至最后一个类目时，在切换就会回到第一个类目
+     
+   - 添加程序
+   
+     - 想安装至U盘的软件建议放置在UFiles/Softwares目录下
+   
+     - 点击时会弹出输入框，可以选择U盘UFiles文件夹下的程序exe或bat进行添加
+     - 填入绝对路径之后会自动转为保存相对路径
+     - 程序名称就是程序的别名，可以自由更改，将显示在列表中
+     - 程序类型就是外部的分类，将会影响软件快捷方式所存在的类目栏
+     - 程序备注填写后会显示在程序列表中
+     - 本地配置的地址是用于程序配置备份的，不填则视为无配置
+     - ![p6.png](https://s2.loli.net/2025/01/15/jHMtJTh8mxZNdSP.jpg)
+   
+   - 管理程序
+     - 先勾选软件列表中的一项软件，点击管理程序会出现修改框
+     - 修改框中的程序路径直接是相对路径，无需修改为绝对路径
+     - 修改当前类目后点击修改程序即可完成修改
+     - 修改过程中如果不想修改了可以点击内容复位恢复原本配置
+     - ![p7.png](https://s2.loli.net/2025/01/15/IWkiJQ3NfVrFh8o.jpg)
+     
+   - 删除程序
+     - 此操作会删除程序列表中所选中的所有程序
+     
+   - 管理员
+     - 点击后进行提权操作，UCatch将以管理员权限打开
+     - 提权后打开软件、备份等操作将以管理员权限执行
+     - 已经提权后无需继续提权，管理员情况下再点击管理员按钮无效
+     
+   - 移除无效
+     - 此操作会扫描所有程序对应程序地址是否有效，将无效程序清除
+     - 移除时应该谨慎操作
+     
+   - 启动程序
+     - 打开在列表中勾选的所有软件
+     - 直接双击列表中的程序可可以打开单个程序
+     - 不勾选则会弹窗提示
+   
+2. 快捷文件
+   - 快捷打开文件
+     - 可以将U盘内UCatch文件夹内的任何文件
+     - 添加文件、删除文件、管理文件修改的是当前目录中的快捷选项，不会影响原文件
+     - 双击文件或勾选多个文件点击打开文件都可以快捷打开对应文件
+   - 文件定时备份
+     - 默认打开软件时不自动执行备份
+     - 双击条目开启对应项定时自动备份
+     - 自动备份执行时右下角托盘会弹出提示，这时请勿关闭程序或拔出U盘
+     - 自动备份时如果文件处于被占用或者不可访问状态时，本次备份会跳过
+     - 当然可以点击立即备份来备份当前列表中所勾选项目
+     - 添加备份时指定路径可以是文件也可以是文件夹，会自动识别
+     - ![p8.png](https://s2.loli.net/2025/01/15/ihnwVFDje6m9N4y.jpg)
+   - 程序配置备份
+     - 添加快捷程序时如果配置了本地配置的目录或文件，则会显示在程序配置备份页面
+     - 点击清除备份会删除U盘中本程序对应的备份
+     - 点击立即备份时会将程序对应的本地配置文件或目录备份到Backups/ConfigBackups目录下
+     - 双击表格中的程序类目会打开配置备份到的目录
+     - 点击恢复备份的时，可以选择多个程序，需要填入配置文件要恢复到的目录的上级目录
+     - 备份为异步操作，恢复为同步操作（需要等待弹出提示，如果窗口卡住需耐心等待）
+     - 备份和恢复时如遇失败，请检查文件是否被占用或文件是否存在等问题
+     - 点击打开目录时会打开配置备份的总目录
+     - ![p9.png](https://s2.loli.net/2025/01/15/15gNcTLDfWMp7h9.jpg)
+
+### 四、程序激活
+
+- 激活码请前往淘宝店铺“异猫工作群”购买
+- 要进行激活操作请到快捷文件功能区点击激活
+- 激活后将支持当前所有功能以及以后更新的所有功能
+- 激活状态绑定于U盘，在任何电脑插入都可使用激活后功能
+- 本软件不存在强制更新，获取新版本可以前往官网下载
+- 更多详细信息请查看用户协议
+
+### 五、注意事项
+
+- 您可以使用其他U盘美化美化方式或软件美化您的U盘，只要不破坏程序文件即可
+- 当然您也可以配置插入U盘时自动打开本程序以实现即插即用
+
+- U盘中的UFiles文件夹是本程序指定操作目录，请勿破坏其中的配置文件（.config）
+- U盘中的程序在运行时如果拔出U盘，程序将自动关闭，注意插紧U盘，以免数据丢失
+- 程序提示正在备份时切建议勿拔出U盘，以免备份失败
+
+        
+        `).toString())});const E=[{version:"1.0.20250115",architecture:"x64",info:"程序/EXE",md5:"b0a625308ccb0dcac2e8152210a9c017",sha1:"40072c127702b3fe27d6667221c94b5eaed952e4",download:{lanzou:"https://shuntaoyuan.lanzout.com/iTf9a2l0cewh",baidu:"https://pan.baidu.com/s/17k9tVjc6yXus7moJwsWZQA?pwd=8yw8"}},{version:"1.0.20250128",architecture:"x64",info:"程序/EXE",md5:"d163e03f46ebf81a0944964d7b9d0489",sha1:"fa8e500fa60fc6fe422c48e0130ae431258985b5",download:{direct:"https://mutantcat.131.996h.cn/index.php/file/get?key=mutantcat&name=Ucatch1.0.20250128x64.exe",lanzou:"https://shuntaoyuan.lanzout.com/iuzY02m5kgja",baidu:"https://pan.baidu.com/s/1VYO-J6UwSfsaIyxmHoIpQQ?pwd=imfh"}},{version:"1.0.20250128",architecture:"x86",info:"程序/EXE",md5:"9d5f901dd1adf35bb4adf4fe46e5265e",sha1:"2c9ea9aa291666e2e27a2403b86f1fd71cd38f7f",download:{direct:"https://mutantcat.131.996h.cn/index.php/file/get?key=mutantcat&name=Ucatch1.0.20250128x86.exe",lanzou:"https://shuntaoyuan.lanzout.com/ilfHv2qfm6cj",baidu:"https://pan.baidu.com/s/12_7t10RLS8T-skvVHApnrg?pwd=wf73"}}];function j(){const n=document.getElementById("download");n&&n.scrollIntoView({behavior:"smooth"})}function z(){const n=document.getElementById("buy");n&&n.scrollIntoView({behavior:"smooth"})}function V(){const n=document.getElementById("update");n&&n.scrollIntoView({behavior:"smooth"})}function S(){const n=document.getElementById("doc");n&&n.scrollIntoView({behavior:"smooth"})}return(n,s)=>{const f=d("el-col"),c=d("el-button"),x=d("el-row"),r=d("el-table-column"),D=d("el-table"),p=d("el-tab-pane"),y=d("el-tabs"),U=d("el-step"),M=d("el-steps");return m(),$(q,null,[t("div",P,[e(x,{style:{height:"100%"}},{default:o(()=>[e(f,{span:h.value,class:"bg hidden-sm-and-down"},null,8,["span"]),e(f,{span:_.value,style:{display:"flex","flex-direction":"column","align-items":"center","justify-content":"center"}},{default:o(()=>[t("div",null,[e(x,null,{default:o(()=>[e(f,{span:24,style:{display:"flex","flex-direction":"column","align-items":"center"}},{default:o(()=>[W,t("h1",G,u(n.$t("apps.UCatch.UCatch")),1),t("p",J,u(n.$t("apps.UCatch.UCatchInfo")),1)]),_:1}),e(f,{span:24,style:{display:"flex","flex-direction":"row","align-items":"center","justify-content":"center"}},{default:o(()=>[R]),_:1}),e(f,{span:24,style:{display:"flex","flex-direction":"row","align-items":"center","justify-content":"center"}},{default:o(()=>[e(c,{type:"primary",size:"large",style:{margin:"10px"},onClick:s[0]||(s[0]=a=>j())},{default:o(()=>[i(u(n.$t("apps.acquisition")),1)]),_:1}),e(c,{type:"primary",size:"large",style:{margin:"10px"},onClick:s[1]||(s[1]=a=>z())},{default:o(()=>[i(u(n.$t("apps.purchase")),1)]),_:1}),e(c,{class:"hidden-sm-and-down",type:"primary",size:"large",style:{margin:"10px"},onClick:s[2]||(s[2]=a=>V())},{default:o(()=>[i(u(n.$t("apps.versionLog")),1)]),_:1}),e(c,{type:"primary",size:"large",style:{margin:"10px"},onClick:s[3]||(s[3]=a=>S())},{default:o(()=>[i(u(n.$t("apps.document")),1)]),_:1})]),_:1})]),_:1})])]),_:1},8,["span"])]),_:1})]),t("div",O,[Y,e(y,{type:"border-card",class:"downloadbar"},{default:o(()=>[e(p,{label:"Windows"},{default:o(()=>[e(x,null,{default:o(()=>[e(D,{data:E,style:{width:"100%"}},{default:o(()=>[e(r,{prop:"version",label:"版本"}),e(r,{prop:"info",label:"形式"}),e(r,{prop:"architecture",label:"架构"}),e(r,{prop:"md5",label:"MD5"}),e(r,{prop:"sha1",label:"SHA1"}),e(r,{label:"下载"},{default:o(a=>[a.row.download.direct!=null?(m(),w(c,{key:0,type:"button.primary",link:"",onClick:C=>b(L)(a.row.download.direct)},{default:o(()=>[i("直接下载")]),_:2},1032,["onClick"])):v("",!0),a.row.download.baidu!=null?(m(),w(c,{key:1,type:"button.primary",link:"",onClick:C=>b(k)(a.row.download.baidu)},{default:o(()=>[i("百度网盘")]),_:2},1032,["onClick"])):v("",!0),a.row.download.lanzou!=null?(m(),w(c,{key:2,type:"button.primary",link:"",onClick:C=>b(k)(a.row.download.lanzou)},{default:o(()=>[i("蓝奏云")]),_:2},1032,["onClick"])):v("",!0),a.row.download.github!=null?(m(),w(c,{key:3,type:"button.primary",link:"",onClick:C=>b(k)(a.row.download.github)},{default:o(()=>[i("Github")]),_:2},1032,["onClick"])):v("",!0)]),_:1})]),_:1})]),_:1})]),_:1})]),_:1})]),t("div",K,[tt,e(y,{type:"border-card",class:"buybar"},{default:o(()=>[e(p,{label:"免费版"},{default:o(()=>[t("div",null,[t("div",et,[t("div",ot,[lt,nt,t("div",st,[at,t("button",{type:"button",class:"action",onClick:s[4]||(s[4]=a=>j())},"立刻获取 ")])])])])]),_:1}),e(p,{label:"激活版"},{default:o(()=>[t("div",null,[t("div",it,[t("div",dt,[ct,pt,t("div",rt,[ut,t("button",{type:"button",class:"action",onClick:s[5]||(s[5]=a=>b(k)("https://item.taobao.com/item.htm?_u=92094fv71k5f6b&id=877793637890&pisk=gcVxzIfuhgjDxdsd0SWoSsIrliQlrT44zozBscmDCuE8XyL0nAmiCfEUvAOG3jDt6uiuim4mGVM_jkwmc-mM65EzDIAiGlftfyyP7m00iF3_Fodgnrm0eFh4Z-AimilT5kcOxMfhtrzqQfshxmL4Q6GI-KO6jm1SPf0TEr9DRrzq_cTkf927uhIq3L9s5c_-NV0B1f9Xhz_-S2pj1mM6VLgIVfi_fK9WFq0jldObfUtSR28XGViXVUgm5fi_ff_8P0ut1hItVEiM6JIdpc32NDO6173xFT4jVHDYNqHIvrNW1C9ZkxnQl0SEaJf-FPkT_FRterwUb2ZXcMD458E-kfCkhXa8Ck3zG6xxz-47-0apSC4qM8F-iSswLz4-cvhuETSmIjEYLDkfUgrTymq_MYtAqc23rSM8AgA8bAeUSVEAMIZC4bV39ETkxD3MhaQJQdkjzmaRZPi49FTmyDbA6dJZUau-xaQJQdkjz4nhkipwQY7P.&spm=a1z09.2.0.0.23352e8dCHIC5l"))},"立刻获取 ")])])])])]),_:1})]),_:1})]),t("div",ht,[_t,e(y,{type:"border-card",class:"downloadbar"},{default:o(()=>[e(p,{label:"Windows"},{default:o(()=>[t("div",ft,[e(M,{direction:"vertical",active:3},{default:o(()=>[e(U,{title:"1.0.20250112",description:"初始版本，完成基本功能开发"}),e(U,{title:"1.1.20250115",description:"修复打开路径的几个问题"}),e(U,{title:"1.1.20250128",description:"更新托盘图标为圆角图标"})]),_:1})])]),_:1})]),_:1})]),t("div",mt,[bt,e(y,{type:"border-card",class:"downloadbar"},{default:o(()=>[e(p,{label:"功能介绍"},{default:o(()=>[gt]),_:1}),e(p,{label:"用户协议"},{default:o(()=>[yt]),_:1}),e(p,{label:"安全信息"},{default:o(()=>[wt,i(" 哈勃安全报告："),vt,kt,i(" 微步安全报告："),xt,Ut,Ct,i(" 微步安全报告："),jt,It,Et,i(" 微步安全报告："),zt,Vt]),_:1})]),_:1})])],64)}}}),Bt=Z(St,[["__scopeId","data-v-2172f94c"]]);export{Bt as default};
