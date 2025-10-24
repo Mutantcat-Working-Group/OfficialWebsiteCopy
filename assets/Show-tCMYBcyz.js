2ed0ad369388ade328555751cdd5d5918ead9728
孙达明 <83948628+tyza66@users.noreply.github.com>
2025-10-24 22:56:26 +0800

    feat：更新网站

---
+++ assets/Show-tCMYBcyz.js
@@ -0,0 +1,68 @@
+import{_ as B}from"./FishMusic-jrYjhnoo.js";import{d as H,v as V,e as j,r as I,x as N,f as L,c as D,a as o,i as e,w as t,F as U,j as a,o as m,t as h,l as d,k as y,u as g,g as b,q as v,p as q,b as T,_ as P}from"./index--_G4LkE8.js";import{m as W}from"./marked.esm-afrdLNPY.js";const i=f=>(q("data-v-85f7f018"),f=f(),T(),f),R={class:"show"},X=i(()=>o("img",{src:B,style:{width:"300px",height:"300px","border-radius":"30px"}},null,-1)),Z={style:{"font-size":"50px","margin-top":"10px"}},G={style:{"font-size":"20px","margin-top":"10px",color:"rgb(129, 129, 129)"}},O=i(()=>o("p",null,"支持平台：Android",-1)),Y={class:"download",id:"download"},J=i(()=>o("h2",{class:"title"},"产品获取",-1)),K={class:"update",id:"update"},Q=i(()=>o("h2",{class:"title"},"版本日志",-1)),ee={style:{height:"50px"}},te={class:"doc",style:{"margin-bottom":"20px"},id:"doc"},oe=i(()=>o("h2",{class:"title"},"产品文档",-1)),ne=i(()=>o("div",{id:"introduce",class:"markdown-body"},null,-1)),le=i(()=>o("h3",{style:{"margin-bottom":"10px","margin-top":"10px"}},"安全报告 安卓版 1.0.20250311",-1)),ie=i(()=>o("a",{href:"https://www.virustotal.com/gui/file/b1e86728d07193899736b53f6464d62e66a1035e50f7c892c83a926faa1297cc?nocache=1"},"查看报告",-1)),se=i(()=>o("br",null,null,-1)),ae=i(()=>o("a",{href:"https://www.hybrid-analysis.com/sample/b1e86728d07193899736b53f6464d62e66a1035e50f7c892c83a926faa1297cc"},"查看报告",-1)),de=i(()=>o("br",null,null,-1)),re=H({__name:"Show",setup(f){V.useI18n(),j({title:"鱼乐 FishMusic - 自挂载无广告网络音乐播放器",meta:[{name:"description",content:"鱼乐（FishMusic）是一款自挂载的免费无广告网络音乐播放器，支持 NAS、云盘（部分）、网络服务器与 SSU，支持本地歌单导入导出（多端通用），失效资源自动跳过，当前提供 Android 客户端。"},{name:"keywords",content:"鱼乐,FishMusic,网络音乐播放器,自挂载,自部署,自托管,NAS,云盘,SSU,歌单导入导出,Android,无广告,多端"},{property:"og:title",content:"鱼乐 FishMusic - 自挂载无广告网络音乐播放器"},{property:"og:description",content:"自挂载/自托管的网络音乐播放器，支持 NAS/云盘/服务器与 SSU，歌单导入导出，多端通用，Android 可用，免费无广告。"},{property:"og:image",content:"https://s2.loli.net/2025/03/02/IxWZXH5mR6Ed1FY.png"}]});const p=I(12),u=I(12);N(()=>{window.addEventListener("resize",function(){var n=window.innerWidth;n<1e3?(p.value=0,u.value=24):(p.value=12,u.value=12)})}),L(()=>{var n=window.innerWidth;n<1e3?(p.value=0,u.value=24):(p.value=12,u.value=12);let s=document.getElementById("introduce");s!=null&&s!=null&&(s.innerHTML=W.parse(`
+<div align="center">
+    <img src="https://s2.loli.net/2025/03/02/IxWZXH5mR6Ed1FY.png" style="width:100px;"/>
+    <h2>鱼乐</h2>
+</div>
+
+### 一、产品概述
+
+- 自挂载的免费无广的网络音乐播放器
+- 支持NAS、云盘(部分)、网络服务器、SSU
+- 支持本地歌单信息导入导出(多端通用)
+- 支持失效资源自动跳过
+
+### 二、安卓版本
+
+1. 用户界面
+   - 播放界面
+   - <img src="https://s2.loli.net/2025/03/11/MdNDnTzSPftL4j5.jpg" alt="img" width="300">
+   - 管理歌单
+   - <img src="https://s2.loli.net/2025/03/11/G5rZF47fuAdUHn8.jpg" alt="img" width="300">
+   - 设置界面
+   - <img src="https://s2.loli.net/2025/03/11/H8MAVDlszwa3I1O.jpg" alt="img" width="300">
+2. 播放音乐
+   - 初始界面就是音乐列表界面
+   - 长按底部唱片区域展开播放操作模块
+   - 点击左侧切换列表循环和随机模式
+   - 点击音乐列表左侧的播放键可以直接切换音乐
+3. 导入音乐
+   - 点击右上角的音乐列表按钮跳转至歌单管理界面
+   - 在歌单管理界面上方的下拉框选择歌单
+   - 之后在指定歌单中点击添加音乐进行添加
+   - 填写表格点击添加即可，不可重名，图标可以传入base64或图片链接
+4. 切换歌单
+   - 点击右上角的音乐列表按钮跳转至歌单管理界面
+   - 在歌单管理界面上方的下拉框选择歌单
+   - 点击选用歌单即可切换
+5. 管理歌单
+   - 点击右上角的音乐列表按钮跳转至歌单管理界面
+   - 在歌单管理界面点击右上角加号添加歌单
+6. 设置超时时间
+   - 在主界面点击右上角设置图标
+   - 修改超时时间，点击修改即可
+7. 导入导出
+
+   - 在主界面点击右上角设置图标
+   - 点击导出即可导出本机所有配置和歌单信息
+   - 导出的格式是一个长字符串，可复制保存或分享
+8. 补充说明
+   - 在主界面点击右上角设置图标
+   - 在上方的输入框输入完整正确的数据字符串
+   - 注意！操作前尽量先备份自己的数据，输入数据应确保正确
+   - 导入后将清空本地原有的所有配置和歌单信息，引入新的信息
+
+### 三、免责声明
+
+- 用户本地保存的音频来源与本工具无关
+
+- 本应用程序不对第三方内容和应用程序中提供的任何链接或资源负责
+
+- 我们承诺用户数据的处理、存储和保护将严格遵循隐私政策。我们不会与第三方共享用户数据，确保用户信息的安全
+
+- 本应用程序的开发者不对因使用该应用程序而产生的任何直接、间接、偶然、特殊或后果性损害负责
+
+- 本应用程序不保证所提供服务的准确性、可靠性或可用性，对因此而产生的任何问题不承担责任
+
+- 本应用程序的功能、界面和政策可能会在不事先通知的情况下进行更改。我们保留随时修改或删除某些功能的权利
+
+        `).toString())});const C=[{version:"1.0.20250311",architecture:"兼容",info:"安装包/APK",md5:"47853c0136484b06871a38552d942914",sha1:"2664dc1e78ad3ec88851746541e3817070399ed5",download:{lanzou:"https://shuntaoyuan.lanzout.com/imqen2q8zdvg",baidu:"https://pan.baidu.com/s/1BHHcxgvxOXirLsbqPJw22Q?pwd=gaim"}}];function A(){const n=document.getElementById("download");n&&n.scrollIntoView({behavior:"smooth"})}function M(){const n=document.getElementById("update");n&&n.scrollIntoView({behavior:"smooth"})}function z(){const n=document.getElementById("doc");n&&n.scrollIntoView({behavior:"smooth"})}return(n,s)=>{const _=a("el-col"),r=a("el-button"),x=a("el-row"),c=a("el-table-column"),F=a("el-table"),w=a("el-tab-pane"),k=a("el-tabs"),$=a("el-step"),E=a("el-steps");return m(),D(U,null,[o("div",R,[e(x,{style:{height:"100%"}},{default:t(()=>[e(_,{span:p.value,class:"bg hidden-sm-and-down"},null,8,["span"]),e(_,{span:u.value,style:{display:"flex","flex-direction":"column","align-items":"center","justify-content":"center"}},{default:t(()=>[o("div",null,[e(x,{style:{display:"flex","flex-direction":"column","align-items":"center"}},{default:t(()=>[e(_,{span:24,style:{display:"flex","flex-direction":"column","align-items":"center"}},{default:t(()=>[X,o("h1",Z,h(n.$t("apps.FishMusic.FishMusic")),1),o("p",G,h(n.$t("apps.FishMusic.FishMusicInfo")),1)]),_:1}),e(_,{span:24,style:{display:"flex","flex-direction":"row","align-items":"center","justify-content":"center"}},{default:t(()=>[O]),_:1}),e(_,{span:24,style:{display:"flex","flex-direction":"row","align-items":"center","justify-content":"center"}},{default:t(()=>[e(r,{type:"primary",size:"large",style:{margin:"10px"},onClick:s[0]||(s[0]=l=>A())},{default:t(()=>[d(h(n.$t("apps.acquisition")),1)]),_:1}),e(r,{type:"primary",size:"large",style:{margin:"10px"},onClick:s[1]||(s[1]=l=>M())},{default:t(()=>[d(h(n.$t("apps.versionLog")),1)]),_:1}),e(r,{type:"primary",size:"large",style:{margin:"10px"},onClick:s[2]||(s[2]=l=>z())},{default:t(()=>[d(h(n.$t("apps.document")),1)]),_:1})]),_:1})]),_:1})])]),_:1},8,["span"])]),_:1})]),o("div",Y,[J,e(k,{type:"border-card",class:"downloadbar"},{default:t(()=>[e(w,{label:"Android"},{default:t(()=>[e(x,null,{default:t(()=>[e(F,{data:C,style:{width:"100%"}},{default:t(()=>[e(c,{prop:"version",label:"版本"}),e(c,{prop:"info",label:"形式"}),e(c,{prop:"architecture",label:"架构"}),e(c,{prop:"md5",label:"MD5"}),e(c,{prop:"sha1",label:"SHA1"}),e(c,{label:"下载"},{default:t(l=>[l.row.download.direct!=null?(m(),y(r,{key:0,type:"button.primary",link:"",onClick:S=>g(b)(l.row.download.direct)},{default:t(()=>[d("直接下载")]),_:2},1032,["onClick"])):v("",!0),l.row.download.baidu!=null?(m(),y(r,{key:1,type:"button.primary",link:"",onClick:S=>g(b)(l.row.download.baidu)},{default:t(()=>[d("百度网盘")]),_:2},1032,["onClick"])):v("",!0),l.row.download.lanzou!=null?(m(),y(r,{key:2,type:"button.primary",link:"",onClick:S=>g(b)(l.row.download.lanzou)},{default:t(()=>[d("蓝奏云")]),_:2},1032,["onClick"])):v("",!0),l.row.download.github!=null?(m(),y(r,{key:3,type:"button.primary",link:"",onClick:S=>g(b)(l.row.download.github)},{default:t(()=>[d("Github")]),_:2},1032,["onClick"])):v("",!0)]),_:1})]),_:1})]),_:1})]),_:1})]),_:1})]),o("div",K,[Q,e(k,{type:"border-card",class:"downloadbar"},{default:t(()=>[e(w,{label:"Android"},{default:t(()=>[o("div",ee,[e(E,{direction:"vertical",active:2},{default:t(()=>[e($,{title:"1.0.20250311",description:"初始版本，提供了初始功能"})]),_:1})])]),_:1})]),_:1})]),o("div",te,[oe,e(k,{type:"border-card",class:"downloadbar"},{default:t(()=>[e(w,{label:"功能介绍"},{default:t(()=>[ne]),_:1}),e(w,{label:"安全信息"},{default:t(()=>[le,d(" VirusTotal安全报告："),ie,se,d(" Hybrid安全报告："),ae,de]),_:1})]),_:1})])],64)}}}),_e=P(re,[["__scopeId","data-v-85f7f018"]]);export{_e as default};
